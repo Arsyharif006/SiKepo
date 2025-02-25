40d05acfd77b65580db8cd645d436403f15d8e50
Arsyharif006 <arsyha006@gmail.com>
2025-02-25 19:44:47 +0700

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/Arsyharif006/SiKepo.git', // Ganti dengan nama repository GitHub Anda
+  base: '/SiKepo/', // Ganti dengan nama repository GitHub Anda
 });
